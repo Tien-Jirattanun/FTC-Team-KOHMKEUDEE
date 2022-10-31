8b5be9d5780b97fbaa93a51ef73b5dba074c0069
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-10-31 09:57:29 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -12,50 +12,13 @@ public class Main extends OpMode {
 
     @Override
     public void init() {
-        motor0 = hardwareMap.get(DcMotor.class ,"motor0");
-        motor1 = hardwareMap.get(DcMotor.class ,"motor1");
+        
     }
 
     @Override
     public void loop() {
 
-        //check x,y
-        float x = gamepad1.left_stick_x;
-        float y = gamepad1.left_stick_y;
 
-        //button
-        boolean L1 = gamepad1.left_bumper;
-
-        //log
-        System.out.print("x axis : ");
-        System.out.println(x);
-        System.out.print("y axis : ");
-        System.out.println(y);
-        System.out.println("------------------");
-
-
-        //slow mode
-        if(L1 == true){
-           x /= 3;
-           y /= 3;
-        }
-
-        //wheel
-        if(x == 0)
-        {
-            motor0.setPower(y);
-            motor1.setPower(y * -1);
-        }
-        else if(x > 0)
-        {
-            motor0.setPower(y - x);
-            motor1.setPower((y + x) * -1);
-        }
-        else if(x < 0)
-        {
-            motor0.setPower(y + (x * (-1)));
-            motor1.setPower((y - (x * (-1))) * -1);
-        }
 
     }
 }
