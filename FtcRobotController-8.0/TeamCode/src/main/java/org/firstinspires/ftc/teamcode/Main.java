d8d4087658a3e0270b1dee5ec02c74aaa9ec0e8f
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-01 15:22:17 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,61 +1,136 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "control")
-public class Main extends OpMode {
+@TeleOp(name = "control")
+public class Main extends LinearOpMode {
 
     DcMotor motor0;                     //RIGHT motor
     DcMotor motor1;                     //LEFT motor
+    Servo servo0;                       //Base right arm
+    Servo servo1;                       //High Right arm
 
     @Override
-    public void init() {
+    public void runOpMode()
+    {
+
+        //equal to the starter value of the servo
+        byte servoData0 = 0;
+        byte servoData1 = 0;
+
         motor0 = hardwareMap.get(DcMotor.class ,"motor0");
         motor1 = hardwareMap.get(DcMotor.class ,"motor1");
+        servo0 = hardwareMap.get(Servo.class, "servo0");
+        servo1 = hardwareMap.get(Servo.class, "servo1");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            //-------------------------Input-------------------------
+
+            //left joy stick
+            float xL = gamepad1.left_stick_x;
+            float yL = gamepad1.left_stick_y;
+
+            //right joy stick
+            float yR = gamepad1.right_stick_y;
+
+            //button
+            boolean L1 = gamepad1.left_bumper;
+
+            //-------------------------Movement-------------------------
+
+            //slow mode
+            if(L1 == true){
+                xL /= 3;
+                yL /= 3;
+            }
+
+            //wheel
+            if(xL == 0)
+            {
+                motor0.setPower(yL);
+                motor1.setPower(yL * -1);
+            }
+            else if(xL > 0)
+            {
+                motor0.setPower(yL - xL);
+                motor1.setPower((yL + xL) * -1);
+            }
+            else if(xL < 0)
+            {
+                motor0.setPower(yL + (xL * (-1)));
+                motor1.setPower((yL - (xL * (-1))) * -1);
+            }
+
+            //-------------------------Arm-------------------------
+            //Auto arm
+            if(gamepad1.cross)
+            {
+                //reset to the start position
+                servoData0 = 0;
+                servoData1 = 0;
+                servo0.setPosition(0);
+                servo1.setPosition(0);
+            }
+            else if(gamepad1.circle)
+            {
+                //Low junction
+                servoData0 = 0;
+                servoData1 = 0;
+                servo0.setPosition(0);
+                servo1.setPosition(0);
+            }
+            else if(gamepad1.square)
+            {
+                //Medium junction
+                servoData0 = 0;
+                servoData1 = 0;
+                servo0.setPosition(0);
+                servo1.setPosition(0);
+            }
+            else if(gamepad1.triangle)
+            {
+                //High junction
+                servoData0 = 0;
+                servoData1 = 0;
+                servo0.setPosition(0);
+                servo1.setPosition(0);
+            }
+
+            //Manual arm
+            if(gamepad1.right_stick_y > 0){
+                servoData0 += 0.1;
+                servoData1 += 0.1;
+                servo0.setPosition(servoData0);
+                servo1.setPosition(servoData1);
+            }
+            else if(gamepad1.right_stick_y < 0){
+                servoData0 -= 0.1;
+                servoData1 -= 0.1;
+                servo0.setPosition(servoData0);
+                servo1.setPosition(servoData1);
+            }
+
+
+            telemetry.addData("Servo Position", servo0.getPosition());
+            telemetry.addData("Servo Position", servo1.getPosition());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+
+
+
     }
 
-    @Override
-    public void loop() {
-
-        //check x,y
-        float x = gamepad1.left_stick_x;
-        float y = gamepad1.left_stick_y;
-
-        //button
-        boolean L1 = gamepad1.left_bumper;
-
-        //log
-        System.out.print("x axis : ");
-        System.out.println(x);
-        System.out.print("y axis : ");
-        System.out.println(y);
-        System.out.println("------------------");
-
-
-        //slow mode
-        if(L1 == true){
-           x /= 3;
-           y /= 3;
-        }
-
-        //wheel
-        if(x == 0)
-        {
-            motor0.setPower(y);
-            motor1.setPower(y * -1);
-        }
-        else if(x > 0)
-        {
-            motor0.setPower(y - x);
-            motor1.setPower((y + x) * -1);
-        }
-        else if(x < 0)
-        {
-            motor0.setPower(y + (x * (-1)));
-            motor1.setPower((y - (x * (-1))) * -1);
-        }
-
-    }
 }
