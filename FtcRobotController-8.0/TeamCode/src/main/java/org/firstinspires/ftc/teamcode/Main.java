5485f83aabe215d3dd1ede2900802339bdfc17c6
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-11-03 17:36:36 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "control")
@@ -33,8 +34,8 @@ public class Main extends LinearOpMode {
         //motor define
         motor0 = hardwareMap.get(DcMotor.class ,"motor0");
         motor1 = hardwareMap.get(DcMotor.class ,"motor1");
-        motor2 = hardwareMap.get(DcMotor.class ,"motor2");
-        motor3 = hardwareMap.get(DcMotor.class ,"motor3");
+        motor2 = hardwareMap.get(DcMotorEx.class ,"motor2");
+        motor3 = hardwareMap.get(DcMotorEx.class ,"motor3");
 
         servo0 = hardwareMap.get(Servo.class ,"servo0");
         servo1 = hardwareMap.get(Servo.class ,"servo1");
@@ -48,9 +49,9 @@ public class Main extends LinearOpMode {
         motor1.setDirection(DcMotor.Direction.REVERSE);
         //Arm setting
         motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
