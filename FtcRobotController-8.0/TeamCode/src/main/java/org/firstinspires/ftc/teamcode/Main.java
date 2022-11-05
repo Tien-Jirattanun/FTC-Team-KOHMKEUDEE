d6204ab360e5c47c249ed69439b017514a282bf2
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-05 13:31:30 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,60 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "control")
-public class Main extends OpMode {
+@Autonomous
+public class Main extends LinearOpMode {
 
-    DcMotor motor0;                     //RIGHT motor
-    DcMotor motor1;                     //LEFT motor
-
-    @Override
-    public void init() {
-        motor0 = hardwareMap.get(DcMotor.class ,"motor0");
-        motor1 = hardwareMap.get(DcMotor.class ,"motor1");
-    }
+    DcMotor motor0;
+    DcMotor motor1;
 
     @Override
-    public void loop() {
+    public void runOpMode(){
 
-        //check x,y
-        float x = gamepad1.left_stick_x;
-        float y = gamepad1.left_stick_y;
+        motor0 = hardwareMap.get(DcMotor.class,"motor0");
+        motor1 = hardwareMap.get(DcMotor.class,"motor1");
 
-        //button
-        boolean L1 = gamepad1.left_bumper;
-
-        //log
-        System.out.print("x axis : ");
-        System.out.println(x);
-        System.out.print("y axis : ");
-        System.out.println(y);
-        System.out.println("------------------");
+        waitForStart();
 
 
-        //slow mode
-        if(L1 == true){
-           x /= 3;
-           y /= 3;
-        }
-
-        //wheel
-        if(x == 0)
-        {
-            motor0.setPower(y);
-            motor1.setPower(y * -1);
-        }
-        else if(x > 0)
-        {
-            motor0.setPower(y - x);
-            motor1.setPower((y + x) * -1);
-        }
-        else if(x < 0)
-        {
-            motor0.setPower(y + (x * (-1)));
-            motor1.setPower((y - (x * (-1))) * -1);
+        if (opModeIsActive()){
+            motor0.setPower(1);
+            motor1.setPower(1);
+            sleep(1000);
+            motor0.setPower(0);
+            motor1.setPower(0);
+            sleep(1000);
         }
 
     }
