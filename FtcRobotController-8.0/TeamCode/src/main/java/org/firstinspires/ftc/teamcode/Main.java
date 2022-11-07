d8645b93ce83877460a35a9b15335690745acfe5
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-07 17:39:24 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -2,31 +2,50 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous
+@TeleOp
 public class Main extends LinearOpMode {
 
-    DcMotor motor0;
-    DcMotor motor1;
+    Servo servo0;
+    Servo servo1;
+
+    double servoPosition0 = 0.5;
 
     @Override
     public void runOpMode(){
+        servo0 = hardwareMap.get(Servo.class, "servo0");
+        servo1 = hardwareMap.get(Servo.class, "servo1");
 
-        motor0 = hardwareMap.get(DcMotor.class,"motor0");
-        motor1 = hardwareMap.get(DcMotor.class,"motor1");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        servo0.setPosition(0.5);
 
         waitForStart();
 
+        while(opModeIsActive()){
 
-        if (opModeIsActive()){
-            motor0.setPower(1);
-            motor1.setPower(1);
-            sleep(1000);
-            motor0.setPower(0);
-            motor1.setPower(0);
-            sleep(1000);
+            if(gamepad1.dpad_up && servoPosition0 < 1){
+                servoPosition0 = servoPosition0 + 0.01;
+                sleep(50);
+
+            }
+            else if(gamepad1.dpad_down && servoPosition0 > 0){
+                servoPosition0 = servoPosition0 - 0.01;
+                sleep(50);
+            }
+
+            servo0.setPosition(servoPosition0);
+
+            telemetry.addData("servo0: ", servo0.getPosition());
+            telemetry.addData("servo0 num : ", servoPosition0);
+            telemetry.addData("Status", "Running");
+            telemetry.update();
         }
+
 
     }
 }
