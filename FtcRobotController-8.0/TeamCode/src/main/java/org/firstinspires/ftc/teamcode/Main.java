588006b34ce996945ad07daa590dc64efc89a3ca
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-07 17:48:17 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,47 +1,34 @@
+//for motor code
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Main extends LinearOpMode {
 
-    Servo servo0;
-    Servo servo1;
+    DcMotorEx motor2;
+    DcMotorEx motor3;
 
     double servoPosition0 = 0.5;
 
     @Override
     public void runOpMode(){
-        servo0 = hardwareMap.get(Servo.class, "servo0");
-        servo1 = hardwareMap.get(Servo.class, "servo1");
+        motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
+        motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-        servo0.setPosition(0.5);
 
         waitForStart();
 
         while(opModeIsActive()){
 
-            if(gamepad1.dpad_up && servoPosition0 < 1){
-                servoPosition0 = servoPosition0 + 0.01;
-                sleep(50);
-
-            }
-            else if(gamepad1.dpad_down && servoPosition0 > 0){
-                servoPosition0 = servoPosition0 - 0.01;
-                sleep(50);
-            }
-
-            servo0.setPosition(servoPosition0);
-
-            telemetry.addData("servo0: ", servo0.getPosition());
-            telemetry.addData("servo0 num : ", servoPosition0);
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
