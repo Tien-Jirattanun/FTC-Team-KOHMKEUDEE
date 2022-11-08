39cc9007ace6d0026c8684697013b5233cfbbc09
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-08 17:56:08 +0700

    SUM

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClass.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClass.java
@@ -1,25 +1,19 @@
-
-//for motor code
-
 package org.firstinspires.ftc.teamcode;
-
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-
 @TeleOp
-public class Main extends LinearOpMode {
+public class testClass extends LinearOpMode {
 
     DcMotorEx motor2;
     DcMotorEx motor3;
 
-    double servoPosition0 = 0.5;
-
     @Override
     public void runOpMode(){
+
         motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
         motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
 
@@ -29,40 +23,13 @@ public class Main extends LinearOpMode {
         motor2.setTargetPosition(0);
         motor3.setTargetPosition(0);
 
-        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-
-        motor2.setVelocity(500);
-        motor3.setVelocity(500);
-
-        motor2.setTargetPosition(1440);
-        motor3.setTargetPosition(1440);
-        armBusy(motor2.isBusy(),motor3.isBusy());
-
-        motor2.setTargetPosition(0);
-        motor3.setTargetPosition(0);
-        armBusy(motor2.isBusy(),motor3.isBusy());
-
-
-        motor2.setTargetPosition(1440);
-        motor3.setTargetPosition(1440);
-        armBusy(motor2.isBusy(),motor3.isBusy());
-
-
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
 
         waitForStart();
 
         while(opModeIsActive()){
-            telemetry.addData("Status", "Running");
-            telemetry.update();
+
         }
-
-
-
 
     }
 
