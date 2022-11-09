c624457e24d960025daf004554a606a949d03340
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-11-09 17:06:32 +0700

    Update Kuay.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kuay.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kuay.java
@@ -4,24 +4,30 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
-public class Main extends LinearOpMode {
+@TeleOp(name = "Kuay")
+public class Kuay extends LinearOpMode {
 
     Servo servo0;
     Servo servo1;
+    Servo servo4;
+    Servo servo5;
 
     double servoPositionMid = 1.0;
     double servoPositionHigh = 1-servoPositionMid;
+    double servoPositionMid1 = 1.0;
 
     @Override
     public void runOpMode(){
         servo0 = hardwareMap.get(Servo.class, "servo0");
         servo1 = hardwareMap.get(Servo.class, "servo1");
+        servo4 = hardwareMap.get(Servo.class, "servo4");
+        servo5 = hardwareMap.get(Servo.class, "servo5");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         servo0.setPosition(servoPositionMid);
+        servo4.setPosition(servoPositionMid);
 
         waitForStart();
 
@@ -40,12 +46,18 @@ public class Main extends LinearOpMode {
 
             }
             else if(gamepad1.dpad_up){
-                if (servoPositionMid > 0 && servoPositionHigh < 0.2 && servoPositionMid > 0){
+                if (servoPositionMid > 0 && servoPositionHigh < 0.2 ){
                     servoPositionHigh = servoPositionHigh + 0.01;
                 }
-                if (servoPositionHigh < 1 && servoPositionHigh >= 0.2 && servoPositionMid > 0){
-                    servoPositionHigh = servoPositionHigh + 0.01;
-                    servoPositionMid = servoPositionMid - 0.01;
+                if (servoPositionHigh >= 0.2){
+                    if(servoPositionHigh < 1){
+                        servoPositionHigh = servoPositionHigh + 0.01;
+                    }
+                    if(servoPositionMid > 0){
+                        servoPositionMid = servoPositionMid - 0.01;
+                    }
+
+
                 }
 
                 sleep(50);
@@ -53,11 +65,14 @@ public class Main extends LinearOpMode {
 
             servo0.setPosition(servoPositionMid);
             servo1.setPosition(servoPositionHigh);
+            servo4.setPosition(servoPositionMid);
+            servo5.setPosition(servoPositionHigh);
 
             telemetry.addData("servo0: ", servo0.getPosition());
             telemetry.addData("servo0 num : ", servoPositionMid);
             telemetry.addData("servo0: ", servo1.getPosition());
             telemetry.addData("servo0 num : ", servoPositionHigh);
+            telemetry.addData("servo4: ", servo4.getPosition());
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
