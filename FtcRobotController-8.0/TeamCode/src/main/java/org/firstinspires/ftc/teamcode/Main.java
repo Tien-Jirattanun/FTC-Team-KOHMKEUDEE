45be667fe3202f0b9ad9ff09a041492a570fd6ac
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-15 14:48:53 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -6,60 +6,43 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "control")
-public class Main extends LinearOpMode {
+@TeleOp(name = "servo")
+public class Motor extends LinearOpMode {
 
     Servo servo0;                       //Mid Right arm
-    Servo servo1;                       //High Right arm
-    Servo servo4;                       //Mid Left arm
-    Servo servo5;                       //High Left arm
-    private ElapsedTime runtime = new ElapsedTime();
+
 
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
 
         //equal to the starter value of the servo
         byte servoData0 = 0;
-        byte servoData1 = 0;
-        byte servoData2 = 0;
-        byte servoData3 = 0;
-
 
         //servo define
         servo0 = hardwareMap.get(Servo.class, "servo0");
-        servo1 = hardwareMap.get(Servo.class, "servo1");
-        servo4 = hardwareMap.get(Servo.class, "servo4");
-        servo5 = hardwareMap.get(Servo.class, "servo5");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
 
         waitForStart();
-        
-        while(true) {
-            runtime.reset();
-            while (opModeIsActive() && (runtime.seconds() <= 3.0)) {
-                servo1.setPosition(1);
+
+        while(opModeIsActive()){
+
+            if(gamepad1.dpad_left){
                 servo0.setPosition(0);
-                servo4.setPosition(1);
-                servo5.setPosition(0);
-                telemetry.addData("Leg 1", runtime.seconds());
-                telemetry.update();
+            }
+            else if(gamepad1.dpad_right){
+                servo0.setPosition(0.3);
             }
 
-            runtime.reset();
-            while (opModeIsActive() && (runtime.seconds() <= 3.0)) {
-                servo1.setPosition(0);
-                servo0.setPosition(1);
-                servo4.setPosition(0);
-                servo5.setPosition(1);
-                telemetry.addData("Leg 2", runtime.seconds());
-                telemetry.update();
-            }
+            telemetry.addData("servo", servo0.getPosition());
+            telemetry.addData("servo : ", servoData0);
+            telemetry.update();
+
         }
+
     }
 
 }
