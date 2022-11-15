2f1bacfe9a82136b74b7e2cd612cf46ae7c39d97
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-15 13:56:15 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,55 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name = "control")
-public class Main extends OpMode {
+@TeleOp(name = "IMU")
+public class Main extends LinearOpMode {
 
-    DcMotor motor0;                     //RIGHT motor
-    DcMotor motor1;                     //LEFT motor
-
-    @Override
-    public void init() {
-        motor0 = hardwareMap.get(DcMotor.class ,"motor0");
-        motor1 = hardwareMap.get(DcMotor.class ,"motor1");
-    }
+    private BNO055IMU imu;
 
     @Override
-    public void loop() {
-        twoWheel();
-    }
+    public void runOpMode() {
+        BNO055IMU.Parameters imuParameters;
+        Orientation angles;
+        Acceleration gravity;
 
-    private void twoWheel(){
-        //check x,y
-        float x = gamepad1.left_stick_x;
-        float y = gamepad1.left_stick_y;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
 
-        //button
-        boolean L1 = gamepad1.left_bumper;
-
-        //slow mode
-        if(L1 == true){
-            x /= 3;
-            y /= 3;
-        }
-
-        //wheel
-        if(x == 0)
-        {
-            motor0.setPower(y);
-            motor1.setPower(y * -1);
-        }
-        else if(x > 0)
-        {
-            motor0.setPower(y - x);
-            motor1.setPower((y + x) * -1);
-        }
-        else if(x < 0)
-        {
-            motor0.setPower(y + (x * (-1)));
-            motor1.setPower((y - (x * (-1))) * -1);
+        // Create new IMU Parameters object.
+        imuParameters = new BNO055IMU.Parameters();
+        // Use degrees as angle unit.
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        // Express acceleration as m/s^2.
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        // Disable logging.
+        imuParameters.loggingEnabled = false;
+        // Initialize IMU.
+        imu.initialize(imuParameters);
+        // Prompt user to press start buton.
+        telemetry.addData("IMU Example", "Press start to continue...");
+        telemetry.update();
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (opModeIsActive()) {
+                // Get absolute orientation
+                // Get acceleration due to force of gravity.
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+                // Display orientation info.
+                telemetry.addData("rot about Z", angles.firstAngle);
+                telemetry.addData("rot about Y", angles.secondAngle);
+                telemetry.addData("rot about X", angles.thirdAngle);
+                // Display gravitational acceleration.
+                telemetry.addData("gravity (Z)", gravity.zAccel);
+                telemetry.addData("gravity (Y)", gravity.yAccel);
+                telemetry.addData("gravity (X)", gravity.xAccel);
+                telemetry.update();
+            }
         }
     }
 }
