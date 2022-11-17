37231432164b66fbd4a9bd657d3602c57e75ae85
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-11-17 12:39:25 +0700

    Update Main.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Controll")
-public class Main extends LinearOpMode {
+public class Test extends LinearOpMode {
 
     DcMotor motor0;                     //RIGHT motor
     DcMotor motor1;                     //LEFT motor
@@ -23,30 +23,54 @@ public class Main extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-
-            boolean pad_up = gamepad1.dpad_up;
-            boolean pad_right = gamepad1.dpad_right;
-            boolean pad_left = gamepad1.dpad_left;
-            boolean pad_down = gamepad1.dpad_down;
+            //joy
             double R = 0.155;
             float X = gamepad1.left_stick_x;
             float Y = gamepad1.left_stick_y;
+            byte A = 0;
+            byte B = 0;
             double W1 = -0.5*X - (sqrt(3)/2)*Y;
             double W2 = -0.5*X + (sqrt(3)/2)*Y  ;
             double W3 = X ;
 
-            motor0.setPower(W2);
-            motor1.setPower(W1);
+            motor0.setPower(W1);
+            motor1.setPower(W2);
+            motor2.setPower(W3);
 
-            if (X>0 || X<0) {
-                sleep(125);
-                motor2.setPower(W3);
+            //dpad
+            if (gamepad1.dpad_up){
+                B=1;
             }
-            else {
-                motor0.setPower(0);
-                motor1.setPower(0);
-                motor2.setPower(0);
+            else if (gamepad1.dpad_down){
+                B=-1;
             }
+            else if (gamepad1.dpad_right){
+                A=1;
+            }
+            else if (gamepad1.dpad_left){
+                A=-1;
+            }
+
+            double M1 = -0.5*A - (sqrt(3)/2)*B;
+            double M2 = -0.5*A + (sqrt(3)/2)*B;
+            double M3 = A ;
+
+            motor0.setPower(M1);
+            motor1.setPower(M2);
+            motor2.setPower(M3);
+
+            //rotate
+            if(gamepad1.left_bumper){
+                motor0.setPower(0.69);
+                motor1.setPower(0.69);
+                motor2.setPower(0.69);
+            }
+            if(gamepad1.right_bumper){
+                motor0.setPower(-0.69);
+                motor1.setPower(-0.69);
+                motor2.setPower(-0.69);
+            }
+
 
         }
     }
@@ -55,61 +79,7 @@ public class Main extends LinearOpMode {
 
 
 
-//    private void twoWheel(){
-//        //check x,y
-//        float x = gamepad1.left_stick_x;
-//        float y = gamepad1.left_stick_y;
-//
-//        //button
-//        boolean L1 = gamepad1.left_bumper;
-//
-//        //slow mode
-//        if(L1 == true){
-//            x /= 3;
-//            y /= 3;
-//        }
-//
-//        //wheel
-//        if(x == 0)
-//        {
-//            motor0.setPower(y);
-//            motor1.setPower(y * -1);
-//        }
-//        else if(x > 0)
-//        {
-//            motor0.setPower(y - x);
-//            motor1.setPower((y + x) * -1);
-//        }
-//        else if(x < 0)
-//        {
-//            motor0.setPower(y + (x * (-1)));
-//            motor1.setPower((y - (x * (-1))) * -1);
-//        }
-//    }
-//}
 
-//if (pad_up) {
-//        motor0.setPower(1);
-//        motor1.setPower(-1);
-//        motor2.setPower(0);
-//        } else if (pad_right) {
-//        motor0.setPower(-0.545);
-//        motor1.setPower(-0.545);
-//        motor2.setPower(1);
-//        } else if (pad_left) {
-//        motor0.setPower(0.545);
-//        motor1.setPower(0.545);
-//        motor2.setPower(-1);
-//        } else if (pad_down) {
-//        motor0.setPower(-1);
-//        motor1.setPower(1);
-//        motor2.setPower(0);
-//        }
-//        else{
-//        motor0.setPower(0);
-//        motor1.setPower(0);
-//        motor2.setPower(0);
-//        }
 
 
 
