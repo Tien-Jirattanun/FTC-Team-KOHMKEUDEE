577eee51911973116de2c204b6d6dcc611013a73
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-11-17 15:02:34 +0700

    Update Test.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,19 +6,26 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "Controll")
+@TeleOp(name = "Controll1")
 public class Test extends LinearOpMode {
 
     DcMotor motor0;                     //RIGHT motor
     DcMotor motor1;                     //LEFT motor
     DcMotor motor2;
+    DcMotor motor3;
 
     @Override
     public void runOpMode() {
         motor0 = hardwareMap.get(DcMotor.class, "motor0");
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
         motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
+
+        motor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor3.setTargetPosition(0);
+//        motor3.
 
         waitForStart();
 
@@ -35,41 +42,29 @@ public class Test extends LinearOpMode {
 
             motor0.setPower(W1);
             motor1.setPower(W2);
-            motor2.setPower(W3);
+            motor2.setPower(W3*0.95);
 
-            //dpad
-            if (gamepad1.dpad_up){
-                B=1;
-            }
-            else if (gamepad1.dpad_down){
-                B=-1;
-            }
-            else if (gamepad1.dpad_right){
-                A=1;
-            }
-            else if (gamepad1.dpad_left){
-                A=-1;
-            }
 
-            double M1 = -0.5*A - (sqrt(3)/2)*B;
-            double M2 = -0.5*A + (sqrt(3)/2)*B;
-            double M3 = A ;
-
-            motor0.setPower(M1);
-            motor1.setPower(M2);
-            motor2.setPower(M3);
-
-            //rotate
+            //arm
             if(gamepad1.left_bumper){
-                motor0.setPower(0.69);
-                motor1.setPower(0.69);
-                motor2.setPower(0.69);
+                motor3.setPower(1);
             }
             if(gamepad1.right_bumper){
-                motor0.setPower(-0.69);
-                motor1.setPower(-0.69);
-                motor2.setPower(-0.69);
+                motor0.setPower(-0.66);
+                motor1.setPower(-0.66);
+                motor2.setPower(-0.66);
             }
+
+            //rotate2.0
+            float C = gamepad1.right_stick_x;
+
+            double R1 = (0.5*C)*0.69;
+            double R2 = (0.5*C)*0.69;
+            double R3 = C*0.69 ;
+
+            motor0.setPower(R1);
+            motor1.setPower(R2);
+            motor2.setPower(R3*0.94);
 
 
         }
