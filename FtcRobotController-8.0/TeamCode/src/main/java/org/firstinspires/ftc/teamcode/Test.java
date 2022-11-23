e29a4da606c6eeaaf44f22c46251a1ce2e841085
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-11-23 15:26:58 +0700

    Update Test.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -12,10 +12,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Control1")
 public class Test extends LinearOpMode {
 
-    DcMotor motor0;                     //RIGHT motor
-    DcMotor motor1;                     //LEFT motor
-    DcMotor motor2;
-    DcMotor motor3;
+    DcMotorEx motor0;                     //RIGHT motor
+    DcMotorEx motor1;                     //LEFT motor
+    DcMotorEx motor2;
+    DcMotorEx motor3;
     //    DcMotor motor3;
     Servo servo1;
     Servo servo0;
@@ -24,10 +24,10 @@ public class Test extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        motor0 = hardwareMap.get(DcMotor.class, "motor0");
-        motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motor0 = hardwareMap.get(DcMotorEx.class, "motor0");
+        motor1 = hardwareMap.get(DcMotorEx.class, "motor1");
+        motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
+        motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
         servo1 = hardwareMap.get(Servo.class, "servo1");
         servo0 = hardwareMap.get(Servo.class, "servo0");
 
@@ -40,81 +40,68 @@ public class Test extends LinearOpMode {
         while (opModeIsActive()) {
 
             //joy
-//            double R = 0.155;
-//            float X = gamepad1.left_stick_x;
-//            float Y = gamepad1.left_stick_y;
-//            byte A = 0;
-//            byte B = 0;
-//            double W1 = -0.5*X - (sqrt(3)/2)*Y;
-//            double W2 = -0.5*X + (sqrt(3)/2)*Y  ;
-//            double W3 = X ;
-//            int targetMotorPosition = 0;
+
+//            if (gamepad1.dpad_left && gamepad1.dpad_up){
+//                motor0.setPower(0);
+//                motor1.setPower(-1);
+//                motor2.setPower(1);
+//                motor3.setPower(0);
+//            }
+//            else if (gamepad1.dpad_left && gamepad1.dpad_down){
+//                motor0.setPower(0);
+//                motor1.setPower(1);
+//                motor2.setPower(-1);
+//                motor3.setPower(0);
+//            }
+//            else if (gamepad1.dpad_right && gamepad1.dpad_up){
+//                motor0.setPower(1);
+//                motor1.setPower(0);
+//                motor2.setPower(0);
+//                motor3.setPower(-1);
+//            }
+//            else if (gamepad1.dpad_right && gamepad1.dpad_down){
+//                motor0.setPower(-1);
+//                motor1.setPower(0);
+//                motor2.setPower(0);
+//                motor3.setPower(1);
+//            }
 //
-//            motor0.setPower(W1);
-//            motor1.setPower(W2);
-//            motor2.setPower(W3*0.95);
-
-            if (gamepad1.dpad_left && gamepad1.dpad_up){
-                motor0.setPower(0);
-                motor1.setPower(-1);
-                motor2.setPower(1);
-                motor3.setPower(0);
-            }
-            else if (gamepad1.dpad_left && gamepad1.dpad_down){
-                motor0.setPower(0);
-                motor1.setPower(1);
-                motor2.setPower(-1);
-                motor3.setPower(0);
-            }
-            else if (gamepad1.dpad_right && gamepad1.dpad_up){
-                motor0.setPower(1);
-                motor1.setPower(0);
-                motor2.setPower(0);
-                motor3.setPower(-1);
-            }
-            else if (gamepad1.dpad_right && gamepad1.dpad_down){
-                motor0.setPower(-1);
-                motor1.setPower(0);
-                motor2.setPower(0);
-                motor3.setPower(1);
-            }
-
-            else if (gamepad1.dpad_up){
-                motor0.setPower(1);
-                motor1.setPower(-1);
-                motor2.setPower(1);
-                motor3.setPower(-1);
-            }
-            else if (gamepad1.dpad_down){
-                motor0.setPower(-1);
-                motor1.setPower(1);
-                motor2.setPower(-1);
-                motor3.setPower(1);
-            }
-            else if (gamepad1.dpad_right){
-                motor0.setPower(1);
-                motor1.setPower(1);
-                motor2.setPower(-1);
-                motor3.setPower(-1);
-            }
-            else if (gamepad1.dpad_left){
-                motor0.setPower(-1);
-                motor1.setPower(-1);
-                motor2.setPower(1);
-                motor3.setPower(1);
-            }
-            else if (gamepad1.cross){
-                motor0.setPower(1);
-                motor1.setPower(1);
-                motor2.setPower(1);
-                motor3.setPower(1);
-            }
-            else if (gamepad1.square){
-                motor0.setPower(-1);
-                motor1.setPower(-1);
-                motor2.setPower(-1);
-                motor3.setPower(-1);
-            }
+//            else if (gamepad1.dpad_up){
+//                motor0.setPower(1);
+//                motor1.setPower(-1);
+//                motor2.setPower(1);
+//                motor3.setPower(-1);
+//            }
+//            else if (gamepad1.dpad_down){
+//                motor0.setPower(-1);
+//                motor1.setPower(1);
+//                motor2.setPower(-1);
+//                motor3.setPower(1);
+//            }
+//            else if (gamepad1.dpad_right){
+//                motor0.setPower(1);
+//                motor1.setPower(1);
+//                motor2.setPower(-1);
+//                motor3.setPower(-1);
+//            }
+//            else if (gamepad1.dpad_left){
+//                motor0.setPower(-1);
+//                motor1.setPower(-1);
+//                motor2.setPower(1);
+//                motor3.setPower(1);
+//            }
+//            else if (gamepad1.cross){
+//                motor0.setPower(1);
+//                motor1.setPower(1);
+//                motor2.setPower(1);
+//                motor3.setPower(1);
+//            }
+//            else if (gamepad1.square){
+//                motor0.setPower(-1);
+//                motor1.setPower(-1);
+//                motor2.setPower(-1);
+//                motor3.setPower(-1);
+//            }
 
             //arm
 //            motor3.setVelocity(200);
