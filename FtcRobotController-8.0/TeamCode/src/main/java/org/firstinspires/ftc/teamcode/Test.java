b6db1741d586da6255a290a065edb85b18bc3ed3
Boonyasit Boonchayaanant <70901393+boongood@users.noreply.github.com>
2022-11-24 17:38:31 +0700

    Update Test.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -16,7 +16,7 @@ public class Test extends LinearOpMode {
     DcMotorEx motor1;                     //LEFT motor
     DcMotorEx motor2;
     DcMotorEx motor3;
-//    DcMotorEx motor4;
+    DcMotor motor4;
     Servo servo1;
     Servo servo0;
 
@@ -27,14 +27,15 @@ public class Test extends LinearOpMode {
         motor1 = hardwareMap.get(DcMotorEx.class, "motor1");
         motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
         motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
-//        motor4 = hardwareMap.get(DcMotorEx.class, "motor4");
-//        servo1 = hardwareMap.get(Servo.class, "servo1");
-//        servo0 = hardwareMap.get(Servo.class, "servo0");
+        motor4 = hardwareMap.get(DcMotor.class, "motor4");
+        servo1 = hardwareMap.get(Servo.class, "servo1");
+        servo0 = hardwareMap.get(Servo.class, "servo0");
 
         motor0.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         motor1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         motor2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         motor3.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        //motor4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
         motor0.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
@@ -42,17 +43,24 @@ public class Test extends LinearOpMode {
         motor2.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         motor3.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
-        double motorVelocity0 = 1440 * 3;
-        double motorVelocity1 = -1440 * 3;
+
+        double motorVelocity0 = 1440;
+        double motorVelocity1 = -1440;
         double motorVelocity2 = 0;
         int encoderData = 0;
 
+//        motor4.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         waitForStart();
 
-        motor3.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+//        motor4.setVelocity(200);
+
 
         while (opModeIsActive()) {
+
+
+
             if (gamepad1.dpad_up){
                 motor0.setVelocity(motorVelocity1);
                 motor1.setVelocity(motorVelocity0);
@@ -77,6 +85,22 @@ public class Test extends LinearOpMode {
                 motor0.setVelocity(motorVelocity1);
                 motor2.setVelocity(motorVelocity0);
             }
+
+
+            //rotate
+            else if(gamepad1.right_stick_x > 0){
+                motor0.setVelocity(motorVelocity1);
+                motor1.setVelocity(motorVelocity1);
+                motor2.setVelocity(motorVelocity1);
+                motor3.setVelocity(motorVelocity1);
+            }
+            else if(gamepad1.right_stick_x < 0){
+                motor0.setVelocity(motorVelocity0);
+                motor1.setVelocity(motorVelocity0);
+                motor2.setVelocity(motorVelocity0);
+                motor3.setVelocity(motorVelocity0);
+            }
+
             else {
                 motor0.setVelocity(motorVelocity2);
                 motor1.setVelocity(motorVelocity2);
@@ -90,29 +114,33 @@ public class Test extends LinearOpMode {
                 servo1.setPosition(0);
             }
             else if(gamepad1.right_bumper){
-                servo0.setPosition(0.6);
-                servo1.setPosition(0.4);
+                servo0.setPosition(0.5);
+                servo1.setPosition(0.5);
             }
 
             //arm
-//            motor3.setVelocity(200);
-//
-//            if(gamepad1.square){
-//                encoderData -= 50;
-//            }
-//            else if(gamepad1.cross && encoderData < 0) {
-//                encoderData += 50;
-//            }
-//            motor3.setTargetPosition(encoderData);
+
+            if(gamepad1.square){
+                motor4.setPower(-1);
+            }
+            else if(gamepad1.cross) {
+                motor4.setPower(1);
+            }
+            else {
+                motor4.setPower(0);
+            }
+
 
             telemetry.addData("thick0 : ", motor0.getCurrentPosition());
             telemetry.addData("thick1 : ", motor1.getCurrentPosition());
             telemetry.addData("thick2 : ", motor2.getCurrentPosition());
             telemetry.addData("thick3 : ", motor3.getCurrentPosition());
+            telemetry.addData("thick arm : ",motor4.getCurrentPosition());
             telemetry.update();
         }
 
     }
+
 }
 
 
