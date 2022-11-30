134eb883e0831240db3e5da5a5e60de60679bc50
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2022-11-30 19:36:13 +0700

    Update Encoder.java

--- FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder.java
+++ FtcRobotController-8.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder.java
@@ -4,11 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
+
 @Autonomous(name = "Encoder")
 public class Encoder extends LinearOpMode {
+
+    int encoderMotor0 = 0;
+    int encoderMotor1 = 0;
+    int encoderMotor2 = 0;
+    int encoderMotor3 = 0;
 
     DcMotorEx motor0;
     DcMotorEx motor1;
@@ -28,6 +35,13 @@ public class Encoder extends LinearOpMode {
         motor2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         motor3.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
+        motor0.setTargetPosition(encoderMotor0);
+        motor1.setTargetPosition(encoderMotor1);
+        motor2.setTargetPosition(encoderMotor2);
+        motor3.setTargetPosition(encoderMotor3);
+
+
+
         waitForStart();
 
         motor0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -39,24 +53,69 @@ public class Encoder extends LinearOpMode {
         motor2.setVelocity(500);
         motor3.setVelocity(500);
 
-        while (opModeIsActive()){
+        if (opModeIsActive()){
             //go backward
 
-            motor0.setTargetPosition(1440);
-            motor1.setTargetPosition(-1440);
-            motor2.setTargetPosition(1440);
-            motor3.setTargetPosition(-1440);
-            sleep(500);
-
-            motor0.setTargetPosition(-1440);
-            motor1.setTargetPosition(-1440);
-            motor2.setTargetPosition(1440);
-            motor2.setTargetPosition(1440);
+            backward(2160);
+            moveRight(6800);
 
 
         }
 
 
+    }
+
+    //60 cm with 2160 thick
+
+    private void backward(int thick){
+        encoderMotor0 += thick;
+        encoderMotor1 -= thick;
+        encoderMotor2 += thick;
+        encoderMotor3 -= thick;
+        motorRun();
+        motorWait();
+    }
+
+    private void forward(int thick){
+        encoderMotor0 -= thick;
+        encoderMotor1 += thick;
+        encoderMotor2 -= thick;
+        encoderMotor3 += thick;
+        motorRun();
+        motorWait();
+    }
+
+    private void moveLeft(int thick){
+        encoderMotor0 += thick;
+        encoderMotor1 += thick;
+        encoderMotor2 -= thick;
+        encoderMotor3 -= thick;
+        motorRun();
+        motorWait();
+    }
+
+    private void moveRight(int thick){
+        encoderMotor0 -= thick;
+        encoderMotor1 -= thick;
+        encoderMotor2 += thick;
+        encoderMotor3 += thick;
+        motorRun();
+        motorWait();
+    }
+
+
+    private void motorRun(){
+        motor0.setTargetPosition(encoderMotor0);
+        motor1.setTargetPosition(encoderMotor1);
+        motor2.setTargetPosition(encoderMotor2);
+        motor3.setTargetPosition(encoderMotor3);
+    }
+
+    private void motorWait(){
+        while (motor0.isBusy() && motor1.isBusy() && motor2.isBusy() && motor3.isBusy()){
+            telemetry.addData("Go", "Robot is running");
+            telemetry.update();
+        }
     }
 
 
